331cbc42acff08b285ff73b9a51f0cc79f27a5a6
Roger Lipscombe <roger@differentpla.net>
2020-07-24 19:50:24 +0100

    Parse opts with clap-rs

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ extern crate lalrpop_util;
 lalrpop_mod!(pub lox);
 
 mod ast;
-mod environment;
-mod interpreter;
 mod bindings;
+mod environment;
 mod error;
+mod interpreter;
 mod value;
 
 mod test;
@@ -15,20 +15,22 @@ use environment::Environment;
 use interpreter::interpret_source;
 use value::Value;
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() <= 1 {
-        eprintln!("{} file.lox", args[0]);
-        return;
-    }
+use clap::Clap;
 
-    let path = &args[1];
-    let source = std::fs::read_to_string(path).expect("read file");
+#[derive(Clap)]
+struct Opts {
+    input: String,
+}
+
+fn main() {
+    let opts = Opts::parse();
+
+    let source = std::fs::read_to_string(&opts.input).expect("read file");
 
     let mut environment = Environment::new();
     bindings::register_globals(&mut environment);
     match interpret_source(&source, &mut environment) {
         Ok(_) => {}
-        Err(e) => error::report_error(&path, &source, e),
+        Err(e) => error::report_error(&opts.input, &source, e),
     };
 }
