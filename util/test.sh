b6a8fccff229c66d40cbda4e48509bf95610f4a6
Roger Lipscombe <roger@differentpla.net>
2020-07-24 20:10:48 +0100

    Test for expected runtime errors

--- util/test.sh
+++ util/test.sh
@@ -3,9 +3,10 @@
 lox=$1
 
 # Use perl to grab the expected output, marked with "// expect: "
-expected=$( perl -ne '/\/\/ expect: (.*)/ && print $1 . "\n"' < $lox )
+expected=$( perl -ne 'print "$1\n" if /\/\/ expect: (.*)/; print "runtime error: $1\n" if /\/\/ expect runtime error: (.*)/;' < "$lox" )
+
 # Run the interpreter (--quiet suppresses the "Compiling/Finished/Running" messages)
-actual=$( cargo run --quiet $lox 2>&1 )
+actual=$( cargo run --quiet -- $lox --simple-errors 2>&1 )
 
 # Compare the expected output to the actual output
 colordiff -u --label "$lox" <(echo "$expected") <(echo "$actual")
