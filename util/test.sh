b95ddc48f71a6989673d0508cb24b5cbf30acc11
Roger Lipscombe <roger@differentpla.net>
2020-07-25 13:22:08 +0100

    Tweak test script

--- util/test.sh
+++ util/test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 lox=$1
+echo "$lox ..."
 
 # Use perl to grab the expected output, marked with "// expect: "
-expected=$( perl -ne 'print "$1\n" if /\/\/ expect: (.*)/; print "runtime error: $1\n" if /\/\/ expect runtime error: (.*)/;' < "$lox" )
+expected=$( perl -ne 'print "$1\n" if /\/\/ expect: (.*)/; print "error: $1\n" if /\/\/ expect runtime error: (.*)/;' < "$lox" )
 
 # Run the interpreter (--quiet suppresses the "Compiling/Finished/Running" messages)
 actual=$( cargo run --quiet -- $lox --simple-errors 2>&1 )
 
 # Compare the expected output to the actual output
-colordiff -u --label "$lox" <(echo "$expected") <(echo "$actual")
+colordiff -u --label "$lox (expected)" <(echo "$expected") --label "$lox (actual)" <(echo "$actual")
